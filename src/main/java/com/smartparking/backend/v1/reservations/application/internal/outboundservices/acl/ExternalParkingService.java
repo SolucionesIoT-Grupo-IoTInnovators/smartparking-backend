04c2ab1ab209f8195addc2f0cf30cde293de4249
JP19-03 <u202014511@upc.edu.pe>
2025-05-03 19:09:49 -0500

    feat(reservations): implement parking acl.

---
+++ src/main/java/com/smartparking/backend/v1/reservations/application/internal/outboundservices/acl/ExternalParkingService.java
@@ -0,0 +1,27 @@
+package com.smartparking.backend.v1.reservations.application.internal.outboundservices.acl;
+
+import com.smartparking.backend.v1.parkingManagement.interfaces.acl.ParkingSpotContextFacade;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ExternalParkingService {
+    private final ParkingSpotContextFacade parkingSpotContextFacade;
+
+    public ExternalParkingService(ParkingSpotContextFacade parkingSpotContextFacade) {
+        this.parkingSpotContextFacade = parkingSpotContextFacade;
+    }
+
+    public Float getParkingRatePerHour(Long parkingId) {
+        return this.parkingSpotContextFacade.getParkingRatePerHour(parkingId);
+    }
+
+    public void updateParkingSpotAvailability(Long parkingId, UUID parkingSpotId, String availability) {
+        this.parkingSpotContextFacade.updateParkingSpotAvailability(parkingId, parkingSpotId, availability);
+    }
+
+    public void updateAvailableSpotsCount(Long parkingId, Integer availableSpots, String operation) {
+        this.parkingSpotContextFacade.updateAvailableParkingSpotCount(parkingId, availableSpots, operation);
+    }
+}
