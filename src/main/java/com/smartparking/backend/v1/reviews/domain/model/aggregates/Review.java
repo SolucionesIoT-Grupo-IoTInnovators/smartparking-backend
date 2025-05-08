936df6213883dc48411a2bfa5d2f20b291889afc
JP19-03 <u202014511@upc.edu.pe>
2025-05-07 20:37:29 -0500

    feat(reviews): add review aggreate class.
    Co-Authored-By: anonimat0 <103579781+anonimat0@users.noreply.github.com>
    Co-Authored-By: Ernesto Zarate <112828936+ern23zc@users.noreply.github.com>

---
+++ src/main/java/com/smartparking/backend/v1/reviews/domain/model/aggregates/Review.java
@@ -0,0 +1,52 @@
+package com.smartparking.backend.v1.reviews.domain.model.aggregates;
+
+import com.smartparking.backend.v1.reviews.domain.model.commands.CreateReviewCommand;
+import com.smartparking.backend.v1.reviews.domain.model.valueobjects.DriverId;
+import com.smartparking.backend.v1.reviews.domain.model.valueobjects.ParkingId;
+import com.smartparking.backend.v1.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Review extends AuditableAbstractAggregateRoot<Review> {
+
+    @Embedded
+    private DriverId driverId;
+
+    @Getter
+    private String driverName;
+
+    @Embedded
+    private ParkingId parkingId;
+
+    @Embedded
+    private String parkingName;
+
+    @Getter
+    private String comment;
+
+    @Getter
+    private Integer rating;
+
+    protected Review() {}
+
+    public Review(CreateReviewCommand command, String driverName, String parkingName) {
+        this.driverId = new DriverId(command.driverId());
+        this.driverName = driverName;
+        this.parkingId = new ParkingId(command.parkingId());
+        this.parkingName = parkingName;
+        this.comment = command.comment();
+        this.rating = command.rating();
+    }
+
+    public Long getDriverId() {
+        return driverId.driverId();
+    }
+
+    public Long getParkingId() {
+        return parkingId.parkingId();
+    }
+}
