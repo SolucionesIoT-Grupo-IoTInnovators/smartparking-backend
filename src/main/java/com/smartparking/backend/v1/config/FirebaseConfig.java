bba84b4d3621354153ea76031a85d9ba77cef04b
Cesar <u202218735@upc.edu.pe>
2025-06-09 20:54:06 -0500

    feat(notifications): add FirebaseConfig

---
+++ src/main/java/com/smartparking/backend/v1/config/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package com.smartparking.backend.v1.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initializeFirebase() {
+        try {
+            InputStream serviceAccount =
+                    getClass().getClassLoader().getResourceAsStream("firebase/iot-innovators-d423f-319c52e7ecc3.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("✅ Firebase inicializado correctamente.");
+            }
+
+        } catch (Exception e) {
+            System.err.println("❌ Error al inicializar Firebase:");
+            e.printStackTrace();
+        }
+    }
+}
